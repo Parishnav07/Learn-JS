0a384b28af7e985fbfbc4178910079a7b67b7c7b
Parishnav Thokal <120368610+Parishnav07@users.noreply.github.com>
2024-01-16 11:14:03 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all the Js Code as the Newer version
+
+// alert(3+3)  // we are using node js not browser
+
+console.log(3  
+    + 3)    // Code readability should be High
+console.log("Parishnav thokal")
+
+// Documentation :
+// // https://developer.mozilla.org/en-US/
+// https://tc39.es/ecma262/
+
+let name = "Parishnav Thokal "
+let age = 19
+let isEligibleToVote = true
+let state;
+
+// number  -->2 to power 53
+// bigint 
+// String -->""
+// boolean --> true / false
+// null    ---> standalone value
+// undefined   
+// Symbol  -->unique
+
+// object 
+
+console.log(typeof "Parishnav")   //--->string
+console.log(typeof age)         //----> number
+console.log(typeof isEligibleToVote)        //-----> boolean
+console.log(typeof null)        // ----> object
+console.log(typeof undefined)    // ----> undefined
+
