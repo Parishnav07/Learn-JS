1b72a7b93cb1d333bca5c48424281514657ba060
Parishnav Thokal <120368610+Parishnav07@users.noreply.github.com>
2024-01-18 14:07:00 +0530

    Operations in JS

---
+++ 01_Basics/03_Operation.js
@@ -0,0 +1,46 @@
+// Operation 
+
+let value = 3
+let negValue = - value
+// console.log(negValue)    ---> -3
+
+// console.log(2+2)        => 4
+// console.log(2-2)        =>0
+// console.log(2 * 2)      =>4
+// console.log(2 / 2)      =>1
+// console.log(2 ** 3)     =>8
+// console.log(2 % 3)      =>2
+
+let str1 = "hello"
+let str2 = " Parishnav "
+let str3 = str1 + str2
+//console.log(str3)   //--> "hello Parishnav"
+
+
+// Operation of Different Datatypes
+
+// console.log("1" + 2)   //=> 12                       /* Not prefered to use this in project*/
+// console.log(1 + "2")  //=> 12
+// console.log("1"+ 2 +2 ) //=> 122                // Read from the documentation
+// console.log(1+2+"2")  //=>32
+// console.log(3 + 4 * 5 % 3)   //=>5
+
+// console.log(true);   //  => True                     //Code should be Clean ,Simple and readable
+// console.log(+true)       //   =>1
+// console.log(true+)    ---> will give error
+
+// console.log(+"");   // ===> 0
+// console.log(+"Parishnav")       //--->Nan         //this type of Code is not used in production
+
+let num1, num2,num3 
+num1 = num2 = num3 = 2+3
+//no Code readabilty
+// no preferred suggested to use 
+
+let gamecounter = 100
+// gamecounter++;console.log(gamecounter); // 101
+// ++gamecounter;console.log(gamecounter);     // 101
+
+// read The Documentation of postfix and prefix mdn docs
+
+
